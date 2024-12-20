fd30ad44d019e0d8e526e26ec0b82f8d723d72ad
denisz98 <deniszaldivarperez98@gmail.com>
2024-12-20 00:43:57 -0500

    cree la funcionalidad del CRUD para el caso de los profesores

--- unissApp/urls.py
+++ unissApp/urls.py
@@ -7,4 +7,11 @@ urlpatterns = [
     path('editar_clase/<int:id_clase>', editar_clase,name="editar_clase"),
     path('eliminar_clase/<int:id_clase>', eliminar_clase,name="eliminar_clase"),
 
+
+    path('profesor/', profesor, name="Profesor"),
+    path('insertar_profesor/', insertar_profesor,name="insertar_profesor"),
+    path('editar_profesor/<int:id_profesor>', editar_profesor,name="editar_profesor"),
+    path('eliminar_profesor/<int:id_profesor>', eliminar_profesor,name="eliminar_profesor"),
+
+
 ]
--- unissApp/views.py
+++ unissApp/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 
 from .models import *
 from .forms import *
-
 def clase(request):
     # buscamos todass las clases en la base de datos
     clases = Clase.objects.all()
@@ -44,3 +43,50 @@ def eliminar_clase(request, id_clase):
 
     return redirect("/clase/")
 
+# ==========================================================Profesor====================================================
+
+
+def profesor(request):
+    profesores = Profesor.objects.all()
+
+    return render(request, 'Profesor/Profesor.html', {"profesor": profesores})
+
+
+def insertar_profesor(request):
+    miformulario = ProfesorForm()
+
+    if request.method == "POST":
+        miformulario = ProfesorForm(data=request.POST)
+        if miformulario.is_valid():
+            miformulario.save()
+
+            return redirect("/insertar_profesor/")
+
+    return render(request, 'Profesor/form.html',
+                  {"miformulario": miformulario})
+
+
+def editar_profesor(request, id_profesor):
+    # obtiene el profesor o muestra un error 404 si no existe
+    profesor = get_object_or_404(Profesor, idProfesor=id_profesor)
+
+    if request.method == "POST":
+        # Rellena el formulario con los datos enviados y la instancia existente
+        miformulario = ProfesorForm(request.POST, instance=profesor)
+        if miformulario.is_valid():
+            # Guarda  cambios
+            miformulario.save()
+            return redirect("/profesor/")
+    else:
+        # Si es una solicitud GET, inicializa el formulario con la instancia existente
+        miformulario = ProfesorForm(instance=profesor)
+
+    return render(request, 'Profesor/form.html', {"miformulario": miformulario})
+
+
+def eliminar_profesor(request, id_profesor):
+    profesor = Profesor.objects.get(pk=id_profesor)
+
+    profesor.delete()
+
+    return redirect("/profesor/")
