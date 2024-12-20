3db95a0e9ffdbde1962c9bda03449ac588667aa2
denisz98 <deniszaldivarperez98@gmail.com>
2024-12-20 00:21:06 -0500

    elimine string innecesario en la url global

--- uniss_2024/urls.py
+++ uniss_2024/urls.py
@@ -20,6 +20,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('unissApp.urls')),
+    path('', include('unissApp.urls')),
 
 ]
