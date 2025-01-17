0ba82e7df36a22f3c892bec68890e44cd25c4f0f
denisz98 <deniszaldivarperez98@gmail.com>
2025-01-17 17:58:23 -0500

    cree los serializadores correspondiente a cada modelo

---
+++ unissApp/serializer.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Disciplina, Profesor, Clase, Estudiante
+
+class DisciplinaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Disciplina
+        fields = '__all__'
+
+class ProfesorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profesor
+        fields = '__all__'
+
+class ClaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clase
+        fields = '__all__'
+
+class EstudianteSerializer(serializers.ModelSerializer):
+    clases = serializers.PrimaryKeyRelatedField(many=True, queryset=Clase.objects.all())
+
+    class Meta:
+        model = Estudiante
+        fields = '__all__'
