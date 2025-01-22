ba3b856d412d3b3d6db94fcf1e73917d6639c9e6
denisz98 <deniszaldivarperez98@gmail.com>
2025-01-22 18:36:04 -0500

    se añadio seguridad de autenticacion requerida a los endpoints declarados

--- unissApp/viewsets.py
+++ unissApp/viewsets.py
@@ -1,19 +1,24 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Disciplina, Profesor, Clase, Estudiante
 from .serializer import DisciplinaSerializer, ProfesorSerializer, ClaseSerializer, EstudianteSerializer
 
 class DisciplinaViewSet(viewsets.ModelViewSet):
     queryset = Disciplina.objects.all()
     serializer_class = DisciplinaSerializer
+    permission_classes = [IsAuthenticated]
 
 class ProfesorViewSet(viewsets.ModelViewSet):
     queryset = Profesor.objects.all()
     serializer_class = ProfesorSerializer
+    permission_classes = [IsAuthenticated]
 
 class ClaseViewSet(viewsets.ModelViewSet):
     queryset = Clase.objects.all()
     serializer_class = ClaseSerializer
+    permission_classes = [IsAuthenticated]
 
 class EstudianteViewSet(viewsets.ModelViewSet):
     queryset = Estudiante.objects.all()
     serializer_class = EstudianteSerializer
+    permission_classes = [IsAuthenticated]
