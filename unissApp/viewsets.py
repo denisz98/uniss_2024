3c1c0672fd0b58a623fbc0d868c3156e37ff0629
denisz98 <deniszaldivarperez98@gmail.com>
2025-01-22 19:39:30 -0500

    se creo un nuevo endpoint para devolver la cantidad de clases en la BD

--- unissApp/viewsets.py
+++ unissApp/viewsets.py
@@ -1,4 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Disciplina, Profesor, Clase, Estudiante
 from .serializer import DisciplinaSerializer, ProfesorSerializer, ClaseSerializer, EstudianteSerializer
@@ -17,6 +22,15 @@ class ClaseViewSet(viewsets.ModelViewSet):
     queryset = Clase.objects.all()
     serializer_class = ClaseSerializer
     permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        operation_description="Obtiene el número total de clases en la base de datos.",
+        responses={200: '{"total_clases": 10}'}
+    )
+    @action(detail=False, methods=['get'])
+    def total_clases(self, request):
+        total = Clase.objects.count()
+        return Response({'total_clases': total})
 
 class EstudianteViewSet(viewsets.ModelViewSet):
     queryset = Estudiante.objects.all()
